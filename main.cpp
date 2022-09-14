36eb66ea1c765bb1d5eaa6e958085aa557d4950c
marpermon <maria.perezmonge@ucr.ac.cr>
2022-09-14 10:56:17 -0600

    i Please enter the commit message for your changes. Lines starting eureuoiiiiiiiiiiiiiiiiiiiiirwe
    j On branch master

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct T_apartamento
+{
+    float largo, ancho;
+}T_apartamento;
+
+typedef struct S_cola_apartamento{
+   T_apartamento*       dato;
+   S_cola_apartamento   *proximo;
+}_cola;
+
+
+class piso
+{
+private
+
+public
+float area_piso, area_efectiva;
+
+}piso;
+
+void piso::
+
+class edificio
+{
+
+}edificio;
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
